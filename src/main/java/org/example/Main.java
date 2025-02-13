8df22368839c08c01a906f8769d05f9b8faf12d5
Davtyan Samvel <dvatyan11@gmail.com>
2025-02-13 18:07:48 +0300

    Revert "long aa"
    This reverts commit 62a34412487d7dd6640d5072cfb14244c752fac7.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,20 @@
 package org.example;
 
 public class Main {
-    private String name = "Hello";
-    private int age = 22;
+   private String name="Hello";
+   private int age=22;
 
 
     public static void main(String[] args) {
-        int a[] = new int[]{1, 3, 3, 45, 54, 66};
-        int max = 0;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > max) {
-                max = a[i];
+       int a[]=new int[]{1,3,3,45,54,66};
+        int max=0;
+        for (int i = 0; i <a.length; i++) {
+            if (a[i]>max){
+                max=a[i];
             }
         }
         System.out.println(max);
-        fjsndlfmls;
+
 
     }
 }
